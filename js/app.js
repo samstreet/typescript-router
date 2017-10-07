0b92b64dbcc489584f0d9357bd64498e95d33cde
Sam Street <samstreet@null.net>
2017-10-07 21:05:13 +0100

    Fully finished TypeScript router based on another library

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var Route = /** @class */ (function () {
-    function Route() {
-        this.fragment = String;
+    function Route(name, fragment, callback) {
+        this.name = name;
+        this.fragment = fragment;
+        if (typeof callback == 'function') {
+            this.callback = callback;
+        }
     }
     Route.prototype.setFragment = function (fragment) {
         this.fragment = fragment;
@@ -18,39 +22,80 @@ var Route = /** @class */ (function () {
     };
     return Route;
 }());
-var Config = /** @class */ (function () {
-    function Config() {
-    }
-    return Config;
-}());
 /**
  * @class App
  * @implements RouterInterface
  */
 var Router = /** @class */ (function () {
     function Router() {
-        this.config = new Config();
         this.routes = new Array();
-        this.route = '/';
+        this.root = '/';
     }
+    Router.prototype.getFragment = function () {
+        var match = window.location.href.match(/#(.*)$/);
+        var fragment = match ? match[1] : '';
+        return this.clean(fragment);
+    };
+    Router.prototype.clean = function (path) {
+        return path.toString().replace(/\/$/, '').replace(/^\//, '');
+    };
+    Router.prototype.check = function (fragment) {
+        var f = fragment || this.getFragment();
+        for (var i = 0; i < this.routes.length; i++) {
+            var frag = this.routes[i].fragment.toString();
+            console.log(frag);
+            var match = f.match(frag);
+            if (match) {
+                match.shift();
+                this.routes[i].callback.apply({}, match);
+                return this;
+            }
+        }
+        return this;
+    };
     Router.prototype.add = function (route) {
         this.routes.push(route);
         return this;
     };
-    Router.prototype.remove = function (route) {
+    Router.prototype.remove = function (name) {
+        for (var i = 0; i < this.routes.length; i++) {
+            if (this.routes[i].name == name) {
+                this.routes.splice(i, 1);
+                return this;
+            }
+        }
         return this;
     };
     Router.prototype.update = function (route) {
         return this;
     };
     Router.prototype.listen = function () {
+        var self = this;
+        var current = self.getFragment();
+        var fn = function () {
+            if (current !== self.getFragment()) {
+                current = self.getFragment();
+                self.check(current);
+            }
+        };
+        clearInterval(this.interval);
+        this.interval = setInterval(fn, 50);
+        return this;
+    };
+    Router.prototype.flush = function () {
+        this.routes = [];
+        this.root = '/';
+        return this;
     };
     Router.prototype.go = function (path) {
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
+        return this;
     };
     return Router;
 }());
 window.onload = function () {
     var router = new Router();
-    router.add(new Route()).add(new Route());
-    console.log(router);
+    router.add(new Route('about', 'about', function () { console.log("about page"); }))
+        .add(new Route('contact', 'contact', function () { console.log("contact page"); }))
+        .listen();
 };
--- typescript/Router.ts
+++ typescript/Router.ts
@@ -2,18 +2,33 @@
 interface RouterInterface {
     add( route: Route );
 
-    remove( route: Route );
+    remove( name: String );
 
     update( route: Route );
 
     listen();
 
+    flush();
+
+    check( fragment: String );
+
     go( path: string );
 }
 
 class Route {
-    fragment = String;
+    fragment: String;
     name: String;
+    callback: Function;
+
+    constructor( name: String, fragment: String, callback: Function  ){
+        this.name = name;
+        this.fragment = fragment;
+
+        if(typeof callback == 'function'){
+            this.callback = callback;
+        }
+
+    }
 
     setFragment( fragment ){
         this.fragment = fragment;
@@ -35,10 +50,6 @@ class Route {
 
 }
 
-class Config {
-
-}
-
 /**
  * @class App 
  * @implements RouterInterface
@@ -46,21 +57,52 @@ class Config {
 class Router implements RouterInterface {
 
     private routes: Array<Route>;
-    private config: Config;
-    private route: String;
+    private root: String;
+    private interval: any;
 
     constructor(){
-        this.config = new Config();
         this.routes = new Array<Route>();
-        this.route  = '/';
+        this.root  = '/';
+    }
+
+    private getFragment(){
+        let match = window.location.href.match(/#(.*)$/);
+        let fragment = match ? match[1] : '';
+        return this.clean( fragment );
+    }
+
+    private clean( path: String ){
+        return path.toString().replace(/\/$/, '').replace(/^\//, '');
+    }
+
+    check( fragment: String ){
+        let f = fragment || this.getFragment();
+        
+        for(let i=0; i<this.routes.length; i++) {
+            let frag = this.routes[i].fragment.toString();
+            console.log(frag);
+            let match = f.match(frag);
+            if(match) {
+                match.shift();
+                this.routes[i].callback.apply({}, match);
+                return this;
+            }           
+        }
+        return this;
     }
     
     add( route: Route ){
-        this.routes.push( route );
+        this.routes.push( route ); 
         return this;
     }
 
-    remove( route: Route ){
+    remove( name: String ){
+        for(var i = 0; i<this.routes.length; i++){
+            if(this.routes[i].name == name){
+                this.routes.splice(i, 1); 
+                return this;
+            }
+        }
         return this;
     }
     
@@ -69,18 +111,37 @@ class Router implements RouterInterface {
     }
     
     listen(){
+        let self = this;
+        let current = self.getFragment();
         
+        let fn = () => {
+            if(current !== self.getFragment()) {
+                current = self.getFragment();
+                self.check(current);
+            }
+        }
+        clearInterval(this.interval);
+        this.interval = setInterval(fn, 50);
+        return this;
+    }
+
+    flush(){
+        this.routes = [];
+        this.root = '/';
+        return this;
     }
     
     go( path: string ){
-
+        window.location.href = window.location.href.replace(/#(.*)$/, '') + '#' + path;
+        return this;
     }
 
 }
 
 window.onload = () => {
     let router = new Router();
-    router.add( new Route() ).add( new Route() );
-
-    console.log(router);
+    
+    router.add( new Route( 'about', 'about', () => { console.log("about page"); }) )
+        .add( new Route( 'contact', 'contact', () => { console.log("contact page"); } ) )
+        .listen(); 
 };
